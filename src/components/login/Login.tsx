0cd8e426d6bf91a51172afcfc765ac5d954efc4c
Hao <74541600+haod98@users.noreply.github.com>
2023-02-02 22:09:20 +0100

    Remove state for now

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -3,7 +3,7 @@ import {Button} from "@mui/material";
 export default function Login() {
     const clientId = `client_id=${process.env.REACT_APP_CLIENT_ID}`
     const redirectUri = `redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`
-    const uri = `https://www.pinterest.com/oauth/?${clientId}&${redirectUri}&response_type=code&scope=boards:read&state=randomString`
+    const uri = `https://www.pinterest.com/oauth/?${clientId}&${redirectUri}&response_type=code&scope=boards:read`
     return (
         <div>
             <h1>Login</h1>
