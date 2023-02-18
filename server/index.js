4340f09531e81e0465bc4f05b99bf4184caf8fc5
Hao <74541600+haod98@users.noreply.github.com>
2023-02-18 21:59:07 +0100

    Init call to pinterest

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
+require("dotenv").config();
 const express = require('express');
-
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
-app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
+const CLIENT_ID = `client_id=${process.env.CLIENT_ID}`
+const REDIRECT_URI = `redirect_uri=${process.env.REDIRECT_URI}`
+const uri = `https://www.pinterest.com/oauth/?${CLIENT_ID}&${REDIRECT_URI}&response_type=code&scope=boards:read`
+
+
+app.get("/login", (req, res) => {
+    res.redirect(`${uri}`);
 });
 
 app.listen(PORT, () => {
--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,15 +1,10 @@
-import {Button} from "@mui/material";
+import React from "react";
 
 export default function Login() {
-    const clientId = `client_id=${process.env.REACT_APP_CLIENT_ID}`
-    const redirectUri = `redirect_uri=${process.env.REACT_APP_REDIRECT_URI}`
-    const uri = `https://www.pinterest.com/oauth/?${clientId}&${redirectUri}&response_type=code&scope=boards:read`
     return (
         <div>
             <h1>Login</h1>
-            <Button variant={"contained"} href={uri}>
-                Login
-            </Button>
+            <a href="http://localhost:3001/login">Login</a>
         </div>
     )
 }
