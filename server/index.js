1dc1350e285087c322d0b4a9723c5fc49acc4ec0
Hao <74541600+haod98@users.noreply.github.com>
2023-02-19 12:22:37 +0100

    Get access token

--- server/index.js
+++ server/index.js
@@ -1,18 +1,36 @@
-require("dotenv").config();
+require('dotenv').config();
 const express = require('express');
+const axios = require('axios');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const CLIENT_ID = `client_id=${process.env.CLIENT_ID}`
-const REDIRECT_URI = `redirect_uri=${process.env.REDIRECT_URI}`
-const uri = `https://www.pinterest.com/oauth/?${CLIENT_ID}&${REDIRECT_URI}&response_type=code&scope=boards:read`
+const CLIENT_ID = `client_id=${process.env.CLIENT_ID}`;
+const REDIRECT_URI = `redirect_uri=${process.env.REDIRECT_URI}`;
+const uri = `https://www.pinterest.com/oauth/?${CLIENT_ID}&${REDIRECT_URI}&response_type=code&scope=boards:read`;
 
+app.get('/login', (req, res) => {
+  res.redirect(`${uri}`);
+});
 
-app.get("/login", (req, res) => {
-    res.redirect(`${uri}`);
+app.get('/callback', (req, res) => {
+  axios
+    .post(
+      'https://api.pinterest.com/v5/oauth/token',
+      {
+        code: req.query.code,
+        redirect_uri: process.env.REDIRECT_URI,
+        grant_type: 'authorization_code',
+      },
+      {
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          Authorization: `Basic ${process.env.BASE64}`,
+        },
+      }
+    )
+    .then((res) => console.log(res));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
-
