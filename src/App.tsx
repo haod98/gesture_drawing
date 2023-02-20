c687049e677c34375e717320c88c73aa0d069df8
Hao <74541600+haod98@users.noreply.github.com>
2023-02-20 19:21:31 +0100

    Add Board component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
-import Login from "./components/login/Login";
+import Board from './components/Board/Board';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './components/login/Login';
 
 function App() {
   return (
-      <div>
-        <h1>Hello World</h1>
-        <Login></Login>
-      </div>
+    <div>
+      <h1>Hello World</h1>
+      <Login />
+      <BrowserRouter>
+        <Routes>
+          <Route path={'/'} element={<Board />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
